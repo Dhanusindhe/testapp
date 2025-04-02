ddfb54bfbfbc8b474fd6a820447546b20e5393f9
Dhanwanth <dhanwanthy@giottus.com>
2025-04-02 17:25:22 +0530

    Notification service integration

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  
   void _incrementCounter() async {
     String? token = await FirebaseMessaging.instance.getToken();
     print("fcm $token");
