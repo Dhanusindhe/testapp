6d4d74331e98f12b281666eeff8f902e58ed2340
Dhanwanth <dhanwanthy@giottus.com>
2025-04-25 22:47:16 +0530

    Final iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -6,33 +6,38 @@ import MoEngageSDK
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-      let sdkConfig = MoEngageSDKConfig(appId: "H0LK4MS1ETIM4ODCRME7R6HE", dataCenter: .data_center_03);
-      sdkConfig.consoleLogConfig = MoEngageConsoleLogConfig(isLoggingEnabled: true, loglevel: .verbose)
-      sdkConfig.appGroupID = "group.com.giottus.testapp.MoEngage"
-      MoEngageInitializer.sharedInstance.initializeDefaultInstance(sdkConfig, launchOptions: launchOptions)
-      FirebaseApp.configure()
-      GeneratedPluginRegistrant.register(with: self)
-      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-      }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+        FirebaseApp.configure()
+        
+        let sdkConfig = MoEngageSDKConfig(appId: "H0LK4MS1ETIM4ODCRME7R6HE", dataCenter: .data_center_03);
+        sdkConfig.consoleLogConfig = MoEngageConsoleLogConfig(isLoggingEnabled: true, loglevel: .verbose)
+        //Below code is for testing purpose need to remove while taking the release build.
+        sdkConfig.updateConfigForTestEnvironment()
+        sdkConfig.appGroupID = "group.com.giottus.testapp.MoEngage"
+        MoEngageInitializer.sharedInstance.initializeDefaultInstance(sdkConfig, launchOptions: launchOptions)
+        MoEngageSDKMessaging.sharedInstance.registerForRemoteNotification(withCategories: nil, andUserNotificationCenterDelegate: self)
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
     override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-      super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-      MoEngageSDKMessaging.sharedInstance.setPushToken(deviceToken)
+        super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+        MoEngageSDKMessaging.sharedInstance.setPushToken(deviceToken)
     }
     override func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-      if #available(iOS 14.0, *) {
-        completionHandler([.sound,.alert, .banner, .list])
-      } else {
-        completionHandler([.sound,.alert])
-      }
+        if #available(iOS 14.0, *) {
+            completionHandler([.sound,.alert, .banner, .list])
+        } else {
+            completionHandler([.sound,.alert])
+        }
     }
-        
+    
     override func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-      super.userNotificationCenter(center, didReceive: response, withCompletionHandler: completionHandler)
-      MoEngageSDKMessaging.sharedInstance.userNotificationCenter(center, didReceive: response)
+        super.userNotificationCenter(center, didReceive: response, withCompletionHandler: completionHandler)
+        MoEngageSDKMessaging.sharedInstance.userNotificationCenter(center, didReceive: response)
     }
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,7 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
     debugPrint('$tag initState() : start ');
     _moengagePlugin.configureLogs(LogLevel.VERBOSE);
     _moengagePlugin.initialise();
-    _moengagePlugin.setUniqueId("123");
+    // _moengagePlugin.setUniqueId("005");
     // _moengagePlugin.registerForPushNotification();
     _moengagePlugin.showInApp();
     _moengagePlugin.showNudge();
@@ -71,56 +71,56 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
     // String? token = await FirebaseMessaging.instance.getToken();
     // print("fcm $token");
     _moengagePlugin.setUserName("Dhanu");
-    _moengagePlugin.setFirstName("Dhanwanth");
-    _moengagePlugin.setLastName("Yashwanth");
-    _moengagePlugin.setEmail("test@gmail.com");
-    _moengagePlugin.setPhoneNumber("123456789");
-    _moengagePlugin.setUserAttribute("int-attr", 0);
-    _moengagePlugin.setUserAttribute("bool-attr", true);
-    _moengagePlugin.setUserAttribute("string-attr", "Some Value");
-    _moengagePlugin.setUserAttribute("double-attr", 10.0);
-    _moengagePlugin.setUserAttribute("int-arr-attr", [100, 200, 300]);
-    _moengagePlugin.setUserAttribute('product', {
-      'item-id': 123,
-      'item-type': 'books',
-      'item-cost': {'amount': 100, 'currency': 'USD'}
-    });
-    _moengagePlugin.setUserAttribute('products', [
-      {
-        'item-id': 123,
-        'item-cost': {'amount': 100, 'currency': 'USD'}
-      },
-      {
-        'item-id': 323,
-        'item-cost': {'amount': 90, 'currency': 'USD'}
-      }
-    ]);
+    _moengagePlugin.setFirstName("Test User");
+    // _moengagePlugin.setLastName("Yashwanth");
+    _moengagePlugin.setEmail("test1@gmail.com");
+    _moengagePlugin.setPhoneNumber("1111111111");
+    // _moengagePlugin.setUserAttribute("int-attr", 0);
+    // _moengagePlugin.setUserAttribute("bool-attr", true);
+    // _moengagePlugin.setUserAttribute("string-attr", "Some Value");
+    // _moengagePlugin.setUserAttribute("double-attr", 10.0);
+    // _moengagePlugin.setUserAttribute("int-arr-attr", [100, 200, 300]);
+    // _moengagePlugin.setUserAttribute('product', {
+    //   'item-id': 123,
+    //   'item-type': 'books',
+    //   'item-cost': {'amount': 100, 'currency': 'USD'}
+    // });
+    // _moengagePlugin.setUserAttribute('products', [
+    //   {
+    //     'item-id': 123,
+    //     'item-cost': {'amount': 100, 'currency': 'USD'}
+    //   },
+    //   {
+    //     'item-id': 323,
+    //     'item-cost': {'amount': 90, 'currency': 'USD'}
+    //   }
+    // ]);
 
-    var properties = MoEProperties();
-    properties
-        .addAttribute("attrString", "String Value")
-        .addAttribute("attrInt", 123)
-        .addAttribute("attrBool", true)
-        .addAttribute("attrDouble", 12.32)
-        .addAttribute("attrLocation", new MoEGeoLocation(12.1, 77.18))
-        .addAttribute("attrArray", ["item1", "item2", "item3"]).addAttribute(
-            'product', {
-      'item-id': 123,
-      'item-type': 'books',
-      'item-cost': {'amount': 100, 'currency': 'USD'}
-    }).addAttribute('products', [
-      {
-        'item-id': 123,
-        'item-cost': {'amount': 100, 'currency': 'USD'}
-      },
-      {
-        'item-id': 323,
-        'item-cost': {'amount': 90, 'currency': 'USD'}
-      }
-    ]).addISODateTime("attrDate", "2019-12-02T08:26:21.170Z");
+    // var properties = MoEProperties();
+    // properties
+    //     .addAttribute("attrString", "String Value")
+    //     .addAttribute("attrInt", 123)
+    //     .addAttribute("attrBool", true)
+    //     .addAttribute("attrDouble", 12.32)
+    //     .addAttribute("attrLocation", new MoEGeoLocation(12.1, 77.18))
+    //     .addAttribute("attrArray", ["item1", "item2", "item3"]).addAttribute(
+    //         'product', {
+    //   'item-id': 123,
+    //   'item-type': 'books',
+    //   'item-cost': {'amount': 100, 'currency': 'USD'}
+    // }).addAttribute('products', [
+    //   {
+    //     'item-id': 123,
+    //     'item-cost': {'amount': 100, 'currency': 'USD'}
+    //   },
+    //   {
+    //     'item-id': 323,
+    //     'item-cost': {'amount': 90, 'currency': 'USD'}
+    //   }
+    // ]).addISODateTime("attrDate", "2019-12-02T08:26:21.170Z");
     // final MoEngageFlutter _moengagePlugin = MoEngageFlutter("H0LK4MS1ETIM4ODCRME7R6HE");
     // _moengagePlugin.initialise();
-    _moengagePlugin.trackEvent('Flutter Event', properties);
+    // _moengagePlugin.trackEvent('Flutter Event', properties);
   }
 
   @override
@@ -141,6 +141,70 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: InkWell(
+                onTap: () async {
+                  _moengagePlugin.identifyUser("Test User 9003");
+                  // var temp = await _moengagePlugin.getUserIdentities();
+                  // print(temp.toString());
+                },
+                child: Container(
+                  width: 100,
+                  color: Colors.blue,
+                  child: Center(
+                    child: Text("User 1"),
+                  ),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                var properties = MoEProperties();
+                properties
+                    .addAttribute("Type", "Anonymous event");
+                    // .addAttribute("Amount", 123)
+                    // .addAttribute("Time", 12.32);
+                _moengagePlugin.trackEvent("Anonymous", properties);
+              },
+              child: Container(
+                width: 100,
+                color: Colors.red,
+                child: Center(
+                  child: Text("Event triger"),
+                ),
+              ),
+            ),
+            // Padding(
+            //   padding: const EdgeInsets.all(10.0),
+            //   child: InkWell(
+            //     onTap: () {
+            //       _moengagePlugin.setUniqueId("009");
+            //     },
+            //     child: Container(
+            //       width: 100,
+            //       color: Colors.green,
+            //       child: Center(
+            //         child: Text("User 3"),
+            //       ),
+            //     ),
+            //   ),
+            // ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: InkWell(
+                onTap: () {
+                  _moengagePlugin.logout();
+                },
+                child: Container(
+                  width: 100,
+                  color: Colors.amber,
+                  child: Center(
+                    child: Text("Logut"),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
